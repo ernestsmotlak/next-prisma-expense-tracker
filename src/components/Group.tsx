422d7ca616f74bd56bc719997d4cf4bdc7295a39
ernestsmotlak <ernestsmotlak@gmail.com>
2024-09-01 15:54:41 +0200

    Added some styles.
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -67,17 +67,42 @@ const Group: React.FC<GroupProps> = ({ groupId }) => {
 
   return (
     <div className="max-w-3xl mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6 text-center">{group.name} Group</h1>
-      <p className="text-xl mb-4">Created by: {group.creator.username}</p>
+      <h1 className="text-3xl font-bold mb-6 text-center underline underline-offset-8">
+        {group.name}
+      </h1>
+      <p className="text-center text-xl mb-4">Created by: {group.creator.username}</p>
 
-      <h2 className="text-2xl font-semibold mb-4">Expenses:</h2>
+      <h2 className="text-center text-2xl font-semibold mb-4">Expenses:</h2>
+      <p className="text-center">Here goes the current state of what you owe/are owed!</p> <br/>
       <ul>
         {group.expenses.map((expense) => (
-          <li key={expense.id} className="mb-4 bg-white shadow-md rounded-lg p-4 border border-gray-200">
-            <strong>{expense.expenseName}</strong><br />
-            <span><strong>Amount Paid:</strong> ${expense.amountPaid.toFixed(2)}</span><br />
-            <span><strong>Paid By:</strong> {expense.paidBy.username}</span><br />
-            <span><strong>Paid For:</strong> {expense.paidFor}</span>
+          <li
+            key={expense.id}
+            className="mx-auto w-full max-w-lg mb-4 bg-white shadow-md rounded-lg p-4 border border-gray-200 flex justify-between items-center bg-green-300"
+          >
+            <div>
+              <strong>{expense.expenseName}</strong>
+              <br />
+              <span>
+                <strong>Amount Paid:</strong> ${expense.amountPaid.toFixed(2)}
+              </span>
+              <br />
+              <span>
+                <strong>Paid By:</strong> {expense.paidBy.username}
+              </span>
+              <br />
+              <span>
+                <strong>Paid For:</strong> {expense.paidFor}
+              </span>
+            </div>
+            <div className="flex flex-col items-center justify-center">
+              <button
+                type="button"
+                className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mb-2"
+              >
+                Return
+              </button>
+            </div>
           </li>
         ))}
       </ul>

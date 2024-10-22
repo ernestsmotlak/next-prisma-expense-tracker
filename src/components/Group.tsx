c8bda325fa90b3282cfd25c0c4d20c33c8c91678
ernestsmotlak <ernestsmotlak@gmail.com>
2024-10-22 18:17:31 +0200

    revert here if proble,
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -91,8 +91,27 @@ const Group: React.FC<GroupProps> = ({ groupId }) => {
             <p className="text-center">
                 Here goes the current state of what you owe/are owed!
             </p>{" "}
-            
-            <AddExpense />
+
+            {/* <AddExpense /> */}
+            <div className="flex justify-center mt-4">
+            <button className="flex items-center bg-purple-200 p-2 rounded-md hover:bg-purple-300">
+                <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6"
+                >
+                    <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M12 4.5v15m7.5-7.5h-15"
+                    />
+                </svg>
+                <span className="ps-2">Expense</span>
+            </button>
+            </div>
             
             <br />
             {!selectedExpense ? (

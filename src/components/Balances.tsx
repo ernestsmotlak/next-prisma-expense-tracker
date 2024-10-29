abf8234284d88c0b6553c6e08d8c64d8ccfa4461
ernestsmotlak <ernestsmotlak@gmail.com>
2024-10-29 14:45:50 +0100

    Working on my own Balance.tsx
    Signed-off-by: ernestsmotlak <ernestsmotlak@gmail.com>

--- src/components/Balances.tsx
+++ src/components/Balances.tsx
@@ -1,5 +1,26 @@
-import { Group } from "next/dist/shared/lib/router/utils/route-regex";
 import React, { useEffect, useState } from "react";
+
+interface Expense {
+    id: number;
+    amountPaid: number;
+    paidFor: string;
+    expenseName: string;
+    paidBy: {
+        id: number;
+        username: string;
+    };
+}
+
+interface Group {
+    id: number;
+    name: string;
+    creatorId: number;
+    creator: {
+        id: number;
+        username: string;
+    };
+    expenses: Expense[];
+}
 
 interface GroupProps {
     group: any;
@@ -7,6 +28,26 @@ interface GroupProps {
 }
 
 const Balances: React.FC<GroupProps> = ({ group, loggedInUser }) => {
+    // console.log(group);
+
+    const amountsArray = group.expenses.map((expense: Expense) => expense.amountPaid);
+    console.log("Amounts Array:", amountsArray);
+
+    const paidForArray = group.expenses.map((expense: Expense) => expense.paidFor.split(','));
+    console.log('PaidForArray: ', paidForArray);
+
+    const myUsername = localStorage.getItem('username');
+
+    const calculate = (amount: number, array: string[]): number[] => {
+        return array.map(() => amount / array.length);
+    };
+
+    const paidForArray2 = group.expenses[0].paidFor.split(",").map((name: string) => name.trim());
+
+    console.log('Here we are: ', calculate(group.expenses[0].amountPaid, paidForArray2));
+    // console.log('Here we are: ' , calculate(group.expenses[0].amountPaid ,group.expenses[0]));
+
+
     return (
         <div className="mt-5 mb-5 p-4 bg-gray-100 rounded-md shadow-md">
             <h1 className="bg-purple-200 text-xl font-semibold">
